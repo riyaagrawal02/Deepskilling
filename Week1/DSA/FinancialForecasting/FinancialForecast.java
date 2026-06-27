f0611359c8719fb02af5ffa383a7df43b3193b9d
riyaagrawal-0025 <riyaagrawal0205@gmail.com>
2026-06-28 01:17:03 +0530

    Implement recursive financial forecasting

---
+++ Week1/DSA/FinancialForecasting/FinancialForecast.java
@@ -0,0 +1,20 @@
+package Week1.DSA.FinancialForecasting;
+public class FinancialForecast {
+    public static double forecast(double currentValue, double growthRate, int years) {
+        if (years == 0) {
+            return currentValue;
+        }
+        return forecast(currentValue * (1 + growthRate), growthRate, years - 1);
+    }
+
+    public static void main(String[] args) {
+
+        double currentValue = 10000;
+        double growthRate = 0.10;
+        int years = 5;
+
+        double futureValue = forecast(currentValue, growthRate, years);
+
+        System.out.printf("Future Value after %d years = %.2f%n", years, futureValue);
+    }
+}
