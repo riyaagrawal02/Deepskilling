c30be7769f07739e1a6d64e2f8239b4a9f7596ef
riyaagrawal-0025 <riyaagrawal0205@gmail.com>
2026-07-06 20:39:52 +0530

    Implement constructor and setter dependency injection

--- SpringCore-Maven/LibraryManagement/src/main/java/com/library/service/BookService.java
+++ SpringCore-Maven/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -6,8 +6,14 @@ public class BookService {
 
     private BookRepository bookRepository;
 
+     public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+        System.out.println("Constructor Injection Executed");
+    }
+
     public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+        System.out.println("Setter Injection Executed");
     }
 
     public void displayService() {
