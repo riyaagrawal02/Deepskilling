049c05dcc84a785e66287803f280cc2a41ab89b5
riyaagrawal-0025 <riyaagrawal0205@gmail.com>
2026-07-23 00:04:07 +0530

    Implement Student Management Portal components

--- React/myfirstreact/src/App.jsx
+++ React/myfirstreact/src/App.jsx
@@ -1,7 +1,13 @@
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+
 function App() {
   return (
     <div>
-      <h1>Welcome to the first session of React</h1>
+      <Home />
+      <About />
+      <Contact />
     </div>
   );
 }
