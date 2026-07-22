085f0c285222bf74cc6cd6eabadf90ff75afb798
riyaagrawal-0025 <riyaagrawal0205@gmail.com>
2026-07-23 02:15:56 +0530

    Implement CalculateScore functional component

---
+++ React/scorecalculatorapp/src/App.jsx
@@ -0,0 +1,14 @@
+import CalculateScore from "./Components/CalculateScore";
+
+function App() {
+  return (
+    <CalculateScore
+      Name="Riya Agrawal"
+      School="GLA University"
+      Total={450}
+      Goal={5}
+    />
+  );
+}
+
+export default App;
