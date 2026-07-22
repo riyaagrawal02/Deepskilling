c0d27a3e1b74893daf0de36a707072e6dbb22770
riyaagrawal-0025 <riyaagrawal0205@gmail.com>
2026-07-23 03:12:53 +0530

    Implement React Router navigation and trainer details

---
+++ React/trainersapp/src/TrainersMock.js
@@ -0,0 +1,32 @@
+import Trainer from "./trainer";
+
+const trainersMock = [
+  new Trainer(
+    "t1",
+    "Syed Khaleelullah",
+    "syed@gmail.com",
+    "9876543210",
+    ".NET",
+    ["C#", "SQL Server", "React", ".NET Core"]
+  ),
+
+  new Trainer(
+    "t2",
+    "Jojo Jose",
+    "jojo@gmail.com",
+    "9876543211",
+    "Java",
+    ["Java", "JSP", "Spring", "Angular"]
+  ),
+
+  new Trainer(
+    "t3",
+    "Elisa Jones",
+    "elisa@gmail.com",
+    "9876543212",
+    "Python",
+    ["Python", "Django", "Angular"]
+  ),
+];
+
+export default trainersMock;
